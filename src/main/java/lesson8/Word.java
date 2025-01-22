1c85c5c4de9c7586c17cdb38ff00f2660965fb6d
tolbist4@gmail.com <AnatoliyStavskiy>
2025-01-22 12:09:08 +0300

    lesson8 fix

--- src/main/java/lesson8/Word.java
+++ src/main/java/lesson8/Word.java
@@ -3,8 +3,20 @@ package lesson8;
 import java.util.*;
 
 public class Word {
-    public Set<String> uniqueWords(String[] words) {
-        return new TreeSet<>(Arrays.asList(words));
+    public List<String> uniqueWords(String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for (String word : map.keySet()) {
+            if (map.get(word) == 1) {
+                list.add(word);
+            }
+        }
+        return list;
     }
 
     public Map<String, Integer> countWords(String[] words) {
