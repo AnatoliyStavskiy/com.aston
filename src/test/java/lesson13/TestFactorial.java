6ff5398119c3fa15dc663f20829b632c713e85ee
tolbist4@gmail.com <AnatoliyStavskiy>
2025-02-11 21:50:15 +0300

    lesson13_testNg

--- src/test/java/lesson13/TestFactorial.java
+++ src/test/java/lesson13/TestFactorial.java
@@ -7,41 +7,43 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class TestFactorial {
 
+    Factorial factorial = new Factorial();
+
     @Test
     @Name("Проверка 0!")
     void testFactorialOfZero() {
-        assertEquals(Factorial.factorialCalculator(0), 1);
+        assertEquals(factorial.factorialCalculator(0), 1);
     }
 
     @Test
     @Name("Проверка 1!")
     void testFactorialOfOne() {
-        assertEquals(Factorial.factorialCalculator(1), 1);
+        assertEquals(factorial.factorialCalculator(1), 1);
     }
 
     @Test
     @Name("Проверка 5!")
     void testPositiveFactorial() {
-        assertEquals(Factorial.factorialCalculator(5), 120);
+        assertEquals(factorial.factorialCalculator(5), 120);
     }
 
     @Test
     @Name("Проверка 20!")
     void testMaxPositiveFactorial() {
-        assertEquals(Factorial.factorialCalculator(20), 2432902008176640000L);
+        assertEquals(factorial.factorialCalculator(20), 2432902008176640000L);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class,
             expectedExceptionsMessageRegExp = "Допустимые значения факториала для типа long 0-20")
     @Name("Проверка -2!")
     void testNegativeFactorial() {
-        Factorial.factorialCalculator(-2);
+        factorial.factorialCalculator(-2);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class,
             expectedExceptionsMessageRegExp = "Допустимые значения факториала для типа long 0-20")
     @Name("Проверка >20!")
-    void testFactorialMoreOfRealType() {
-        Factorial.factorialCalculator(21);
+    public void testFactorialMoreOfRealType() {
+        factorial.factorialCalculator(21);
     }
 }
